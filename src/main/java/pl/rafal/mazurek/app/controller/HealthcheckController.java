828629b51496d77fc0f8e57d75affda93d75dbbf
Rafal Mazurek <rafal.mazurek@docflow.pl>
2019-02-06 14:32:17 +0100

    clean

--- src/main/java/pl/rafal/mazurek/app/controller/HealthcheckController.java
+++ src/main/java/pl/rafal/mazurek/app/controller/HealthcheckController.java
@@ -3,7 +3,7 @@ package pl.rafal.mazurek.app.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
 public class HealthcheckController {
 
     @GetMapping(path = "/healthcheck")
--- src/test/java/pl/rafal/mazurek/app/parser/CSVParserTest.java
+++ src/test/java/pl/rafal/mazurek/app/parser/CSVParserTest.java
@@ -1,7 +1,6 @@
 package pl.rafal.mazurek.app.parser;
 
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockMultipartFile;
